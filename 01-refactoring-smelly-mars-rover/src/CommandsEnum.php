94f5117db2f2739887eb7aed6e19defc6a9ab3c5
unknown <colormess>
2025-05-15 09:22:13 +0300

    lab3 refactoring

---
+++ 01-refactoring-smelly-mars-rover/src/CommandsEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+enum CommandsEnum: string
+{
+    case left = 'L';
+    case right = 'R';
+    case forward = 'F';
+}
--- 01-refactoring-smelly-mars-rover/src/Rover.php
+++ 01-refactoring-smelly-mars-rover/src/Rover.php
@@ -6,68 +6,56 @@ namespace App;
 
 class Rover
 {
-    private string $direction;
-    private int $y;
-    private int $x;
+    const DISPLACEMENT = 1;
+    private Direction $direction;
+    private Coordinates $coordinates;
 
     public function __construct(int $x, int $y, string $direction)
     {
-        $this->direction = $direction;
-        $this->y = $y;
-        $this->x = $x;
+        $this->direction = Direction::create($direction);
+        $this->coordinates = new Coordinates($y, $x);
     }
 
     public function receive(string $commandsSequence): void
     {
-        $commandsSequenceLength = strlen($commandsSequence);
-        for ($i = 0; $i < $commandsSequenceLength; ++$i) {
-            $command = substr($commandsSequence, $i, 1);
-            if ($command === "l" || $command === "r") {
-                // Rotate Rover
-                if ($this->direction === "N") {
-                    if ($command === "r") {
-                        $this->direction = "E";
-                    } else {
-                        $this->direction = "W";
-                    }
-                } else if ($this->direction === "S") {
-                    if ($command === "r") {
-                        $this->direction = "W";
-                    } else {
-                        $this->direction = "E";
-                    }
-                } else if ($this->direction === "W") {
-                    if ($command === "r") {
-                        $this->direction = "N";
-                    } else {
-                        $this->direction = "S";
-                    }
-                } else {
-                    if ($command === "r") {
-                        $this->direction = "S";
-                    } else {
-                        $this->direction = "N";
-                    }
-                }
-            } else {
-                // Displace Rover
-                $displacement1 = -1;
+        $commands = $this->extractCommandsFrom($commandsSequence);
+        $this->processCommands($commands);
 
-                if ($command === "f") {
-                    $displacement1 = 1;
-                }
-                $displacement = $displacement1;
+    }
 
-                if ($this->direction === "N") {
-                    $this->y += $displacement;
-                } else if ($this->direction === "S") {
-                    $this->y -= $displacement;
-                } else if ($this->direction === "W") {
-                    $this->x -= $displacement;
-                } else {
-                    $this->x += $displacement;
-                }
-            }
+    private function processCommand(CommandsEnum $command): void
+    {
+        switch ($command) {
+            case CommandsEnum::left:
+                $this->direction = $this->direction->rotateLeft();
+                break;
+            case CommandsEnum::right:
+                $this->direction = $this->direction->rotateRight();
+                break;
+            case CommandsEnum::forward:
+                $this->coordinates = $this->direction->move(self::DISPLACEMENT, $this->coordinates);
+                break;
+            default:
+                $this->coordinates = $this->direction->move(-self::DISPLACEMENT, $this->coordinates);
+                break;
+        }
+    }
+
+    private function extractCommandsFrom(string $commandsSequence): array
+    {
+        $commandsSequenceLenght = strlen($commandsSequence);
+        $commands = [];
+        for ($i = 0; $i < $commandsSequenceLenght; ++$i) {
+            $commands[] = substr($commandsSequence, $i, 1);
+        }
+        return $commands;
+    }
+
+    private function processCommands(array $commands): void
+    {
+        foreach ($commands as $command) {
+            $command = CommandsEnum::from($command);
+            $this->processCommand($command);
         }
     }
 }
